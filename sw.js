4f0e2e0800b14137c8b4b1a423e4db99fcd2c5b8
naoki matsuyama <naokiwpc@gmail.com>
2026-09-18 20:10:36 +0900

    v0.2: 3Dダンジョン探索・階ごとに難易度が上がる問題・たおすと宝箱

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'kashiko-v1';
+var CACHE = 'kashiko-v2';
 var FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
