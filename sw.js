02a994b649d125bbbd1e29a3061a3d054e28dc11
naoki matsuyama <naokiwpc@gmail.com>
2026-09-20 11:50:54 +0900

    v0.3: 算数だけの塔に国語と英語を混ぜる(階ごとに教科がかわる・選ばせない)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'kashiko-v0-2b';
+var CACHE = 'kashiko-v0-3d';
 var FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
